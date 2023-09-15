06bf4a19d627e49cad39d34248413f04264bfe37
Yanus22 <gag.nazaryan.2001@inbox.ru>
2023-09-15 15:20:04 +0400

    baci sortic mnacac@ ashxatuma

--- src/AnotherNode.java
+++
@@ -1,7 +0,0 @@
-public class AnotherNode {
-    public static void main(String[] args) {
-        Number x = 17;
-        Double f = Double.parseDouble(x+"");
-        System.out.println(f);
-    }
-}
